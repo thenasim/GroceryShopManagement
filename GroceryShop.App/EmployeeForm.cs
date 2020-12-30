7a907836765e3088d1e346c5b56682f309c857c0
ShameemShahriar <sshahriar33@gmail.com>
2020-12-30 21:14:13 +0600

    employee form manage cart feature design done

--- GroceryShop.App/EmployeeForm.cs
+++ GroceryShop.App/EmployeeForm.cs
@@ -126,5 +126,94 @@
         {
             pnlManageCarts.Visible = true;
         }
+
+        //Search Inventory button hover color
+        private void btnSearchInventory_MouseEnter(object sender, EventArgs e)
+        {
+            btnSearchInventory.IconColor = Color.LightBlue;
+        }
+
+        //Search Inventory button hover color
+        private void btnSearchInventory_MouseLeave(object sender, EventArgs e)
+        {
+            btnSearchInventory.IconColor = Color.White;
+        }
+
+        //ADD button hover color
+        private void btnADD_MouseEnter(object sender, EventArgs e)
+        {
+            btnADD.ForeColor = Color.SkyBlue;
+            btnADD.FlatAppearance.BorderColor = Color.SkyBlue;
+        }
+
+        //ADD button hover color
+        private void btnADD_MouseLeave(object sender, EventArgs e)
+        {
+            btnADD.ForeColor = Color.White;
+            btnADD.FlatAppearance.BorderColor = Color.White;
+        }
+
+        //Clear button hover color
+        private void btnClear_MouseEnter(object sender, EventArgs e)
+        {
+            btnClear.ForeColor = Color.Red;
+            btnClear.FlatAppearance.BorderColor = Color.Red;
+        }
+
+        //Clear button hover color
+        private void btnClear_MouseLeave(object sender, EventArgs e)
+        {
+            btnClear.ForeColor = Color.White;
+            btnClear.FlatAppearance.BorderColor = Color.White;
+        }
+
+        //Searchbar watermark
+        private void txtSearchbar_Enter(object sender, EventArgs e)
+        {
+            if(txtSearchbar.Text=="Search here")
+            {
+                txtSearchbar.Text = "";
+                txtSearchbar.ForeColor = Color.White;
+            }
+        }
+
+        //Searchbar watermark
+        private void txtSearchbar_Leave(object sender, EventArgs e)
+        {
+            if (txtSearchbar.Text == "")
+            {
+                txtSearchbar.Text = "Search here";
+                txtSearchbar.ForeColor = Color.Gray;
+            }
+        }
+
+        //Print invoice button hover color
+        private void btnPrintInvoice_MouseEnter(object sender, EventArgs e)
+        {
+            btnPrintInvoice.ForeColor = Color.Green;
+            btnPrintInvoice.FlatAppearance.BorderColor = Color.Green;
+        }
+
+        //Print invoice button hover color
+        private void btnPrintInvoice_MouseLeave(object sender, EventArgs e)
+        {
+            btnPrintInvoice.ForeColor = Color.White;
+            btnPrintInvoice.FlatAppearance.BorderColor = Color.White;
+
+        }
+
+        //Clear invoice button hover color
+        private void btnClearInvoice_MouseEnter(object sender, EventArgs e)
+        {
+            btnClearInvoice.ForeColor = Color.Red;
+            btnClearInvoice.FlatAppearance.BorderColor = Color.Red;
+        }
+
+        //Clear invoice button hover color
+        private void btnClearInvoice_MouseLeave(object sender, EventArgs e)
+        {
+            btnClearInvoice.ForeColor = Color.White;
+            btnClearInvoice.FlatAppearance.BorderColor = Color.White;
+        }
     }
 }
--- GroceryShop.App/Program.cs
+++ GroceryShop.App/Program.cs
@@ -19,8 +19,8 @@ namespace GroceryShop.App
             //Application.Run(new LoginForm());
             //Application.Run(new Inventory());
             //Application.Run(new Userdetails());
-            Application.Run(new AdminForm());
-            //Application.Run(new EmployeeForm());
+            //Application.Run(new AdminForm());
+            Application.Run(new EmployeeForm());
             //Application.Run(new ManagerForm());
         }
     }
