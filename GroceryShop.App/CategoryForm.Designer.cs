67dc7af27501a1b8171655a126bc62820849f57b
ShameemShahriar <sshahriar33@gmail.com>
2021-01-02 23:48:49 +0600

    category form gridview font color fixed

--- GroceryShop.App/CategoryForm.Designer.cs
+++ GroceryShop.App/CategoryForm.Designer.cs
@@ -58,9 +58,8 @@
             this.pnlFormmove.Controls.Add(this.btnClose);
             this.pnlFormmove.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlFormmove.Location = new System.Drawing.Point(0, 0);
-            this.pnlFormmove.Margin = new System.Windows.Forms.Padding(4);
             this.pnlFormmove.Name = "pnlFormmove";
-            this.pnlFormmove.Size = new System.Drawing.Size(1533, 42);
+            this.pnlFormmove.Size = new System.Drawing.Size(1150, 34);
             this.pnlFormmove.TabIndex = 4;
             this.pnlFormmove.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlFormmove_MouseDown);
             this.pnlFormmove.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlFormmove_MouseMove);
@@ -71,10 +70,9 @@
             this.lblCategory.AutoSize = true;
             this.lblCategory.Font = new System.Drawing.Font("Segoe UI Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCategory.ForeColor = System.Drawing.Color.White;
-            this.lblCategory.Location = new System.Drawing.Point(28, 4);
-            this.lblCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCategory.Location = new System.Drawing.Point(21, 3);
             this.lblCategory.Name = "lblCategory";
-            this.lblCategory.Size = new System.Drawing.Size(127, 32);
+            this.lblCategory.Size = new System.Drawing.Size(102, 25);
             this.lblCategory.TabIndex = 2;
             this.lblCategory.Text = "Category ";
             // 
@@ -86,10 +84,9 @@
             this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMinimize.ForeColor = System.Drawing.Color.White;
-            this.btnMinimize.Location = new System.Drawing.Point(1415, 1);
-            this.btnMinimize.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMinimize.Location = new System.Drawing.Point(1061, 1);
             this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(48, 41);
+            this.btnMinimize.Size = new System.Drawing.Size(36, 33);
             this.btnMinimize.TabIndex = 5;
             this.btnMinimize.Text = "-";
             this.btnMinimize.UseVisualStyleBackColor = false;
@@ -103,10 +100,9 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(1480, 2);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Location = new System.Drawing.Point(1110, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(45, 41);
+            this.btnClose.Size = new System.Drawing.Size(34, 33);
             this.btnClose.TabIndex = 6;
             this.btnClose.Text = "X";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -118,10 +114,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 86);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(21, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 32);
+            this.label1.Size = new System.Drawing.Size(151, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Category Name";
             // 
@@ -131,10 +126,9 @@
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEdit.ForeColor = System.Drawing.Color.White;
-            this.btnEdit.Location = new System.Drawing.Point(731, 82);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEdit.Location = new System.Drawing.Point(548, 67);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(156, 41);
+            this.btnEdit.Size = new System.Drawing.Size(117, 33);
             this.btnEdit.TabIndex = 7;
             this.btnEdit.Text = "EDIT";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -148,10 +142,9 @@
             this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.ForeColor = System.Drawing.Color.White;
-            this.btnClear.Location = new System.Drawing.Point(953, 84);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Location = new System.Drawing.Point(715, 68);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(164, 39);
+            this.btnClear.Size = new System.Drawing.Size(123, 32);
             this.btnClear.TabIndex = 8;
             this.btnClear.Text = "CLEAR";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -159,10 +152,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(251, 91);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(188, 74);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(337, 22);
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
             this.textBox1.TabIndex = 9;
             // 
             // btnShow
@@ -171,10 +163,9 @@
             this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnShow.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnShow.ForeColor = System.Drawing.Color.White;
-            this.btnShow.Location = new System.Drawing.Point(1309, 566);
-            this.btnShow.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShow.Location = new System.Drawing.Point(982, 460);
             this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(156, 41);
+            this.btnShow.Size = new System.Drawing.Size(117, 33);
             this.btnShow.TabIndex = 10;
             this.btnShow.Text = "SHOW";
             this.btnShow.UseVisualStyleBackColor = true;
@@ -187,42 +178,44 @@
             this.pnlGridviewforcategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(43)))), ((int)(((byte)(53)))));
             this.pnlGridviewforcategory.Controls.Add(this.dgvCategory);
             this.pnlGridviewforcategory.Controls.Add(this.btnShow);
-            this.pnlGridviewforcategory.Location = new System.Drawing.Point(24, 154);
-            this.pnlGridviewforcategory.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlGridviewforcategory.Location = new System.Drawing.Point(18, 125);
             this.pnlGridviewforcategory.Name = "pnlGridviewforcategory";
-            this.pnlGridviewforcategory.Size = new System.Drawing.Size(1483, 628);
+            this.pnlGridviewforcategory.Size = new System.Drawing.Size(1112, 510);
             this.pnlGridviewforcategory.TabIndex = 11;
             // 
             // dgvCategory
             // 
             this.dgvCategory.AllowUserToAddRows = false;
             this.dgvCategory.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
-            this.dgvCategory.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dgvCategory.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvCategory.AllowUserToResizeColumns = false;
+            this.dgvCategory.AllowUserToResizeRows = false;
             this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCategory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.categoryid,
             this.name});
             this.dgvCategory.Cursor = System.Windows.Forms.Cursors.Default;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvCategory.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvCategory.Location = new System.Drawing.Point(3, 3);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvCategory.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvCategory.Location = new System.Drawing.Point(4, 2);
+            this.dgvCategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvCategory.MultiSelect = false;
             this.dgvCategory.Name = "dgvCategory";
             this.dgvCategory.ReadOnly = true;
             this.dgvCategory.RowHeadersWidth = 51;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Blue;
+            this.dgvCategory.RowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvCategory.RowTemplate.Height = 24;
             this.dgvCategory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvCategory.Size = new System.Drawing.Size(1462, 511);
-            this.dgvCategory.TabIndex = 11;
+            this.dgvCategory.Size = new System.Drawing.Size(1096, 415);
+            this.dgvCategory.TabIndex = 12;
             this.dgvCategory.DoubleClick += new System.EventHandler(this.dgvCategory_DoubleClick);
             // 
             // categoryid
@@ -245,10 +238,10 @@
             // 
             // CategoryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(43)))), ((int)(((byte)(60)))));
-            this.ClientSize = new System.Drawing.Size(1533, 800);
+            this.ClientSize = new System.Drawing.Size(1150, 650);
             this.Controls.Add(this.pnlGridviewforcategory);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnEdit);
@@ -258,7 +251,6 @@
             this.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CategoryForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.CategoryForm_Load);
